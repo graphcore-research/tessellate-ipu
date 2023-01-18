fec5945d0b605dbd06792cc58eb982caddc9f41c
Paul Balanca <paulb@graphcore.ai>
2023-01-18 10:03:52 +0000

    Demo used in JAX Poplar programming presentation.

---
+++ examples/demo/demo1.py
@@ -0,0 +1,17 @@
+from functools import partial
+import jax
+import numpy as np
+from jax_ipu_research.tile import tile_put_replicated, tile_put_sharded, tile_map_primitive
+
+
+data = np.array([1, -2, 3], np.float32)
+tiles = (0, 2, 5)
+
+@partial(jax.jit, backend="ipu")
+def compute_fn(input):
+    input = tile_put_sharded(input, (0, 2, 5))
+    return tile_map_primitive(jax.lax.neg_p, input)
+
+output = compute_fn(data)
+print(output)
+print(output.array)
