52ec07e71e19f70f56c7b654e1e6b106563ce944
Paul Balanca <paulb@graphcore.ai>
2023-02-09 20:18:18 +0000

    Remove Git LFS from the repository. Restoring LFS files as normal git. (#53)

--- jax_ipu_research/external/fastbase64/fastavxbase64.h
+++ jax_ipu_research/external/fastbase64/fastavxbase64.h
@@ -1,3 +1,37 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:add721e2f5c2bc3134639da04a8f053ba0d407ca318b25ad741868cd81f8d11c
-size 826
+#ifndef EXPAVX_B64
+#define EXPAVX_B64
+
+/**
+* Assumes recent x64 hardware with AVX2 instructions.
+*/
+
+#include <stddef.h>
+#include <stdint.h>
+#include "chromiumbase64.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+* This code extends Nick Galbreath's high performance base 64decoder (used in Chromium), the API is the
+* same effectively, see chromium64.h.
+*/
+
+/*
+* AVX2 accelerated version of Galbreath's chromium_base64_decode function
+* Usage remains the same, see chromium.h.
+*/
+size_t fast_avx2_base64_decode(char *out, const char *src, size_t srclen);
+
+/*
+* AVX2 accelerated version of Galbreath's chromium_base64_encode function
+* Usage remains the same, see chromium.h.
+*/
+size_t fast_avx2_base64_encode(char* dest, const char* str, size_t len);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
