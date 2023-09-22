434193fa9c1a66e3d67fd75d6ca18b1eadea4279
Paul Balanca <paulb@graphcore.ai>
2023-09-22 12:24:06 +0000

    wip

--- examples/demo/demo_io_tiles.py
+++ examples/demo/demo_io_tiles.py
@@ -22,38 +22,46 @@ def compute_fn(input, iobuffer):
     - `input` gets transfered to IO tiles, and then copied into `iobuffer` at the end of the program;
     - `iobuffer`, with previous call data, is already available on compute tiles for crunching numbers!
     """
+    assert input.shape == iobuffer.shape
 
     # IO tiles => transfer from host (not blocking IO tiles.)
-    input = tile_put_sharded(input, io_tiles)
+    input_sharded = tile_put_sharded(input, io_tiles)
 
     # Compute tiles => use iobuffer already available..
-    iobuffer = tile_put_sharded(iobuffer, compute_tiles)
+    iobuffer_sharded = tile_put_sharded(iobuffer, compute_tiles)
     # Some arbitrary computation...
-    output = iobuffer[:, :64]
-    output = tile_map(jax.lax.sin_p, output)
-    output = tile_map(jax.lax.reduce_sum_p, output, axes=(0,))
+    output = iobuffer_sharded[:, :64]
+    output = tile_map(jax.lax.sin_p, output)  # type:ignore
+    output = tile_map(jax.lax.reduce_sum_p, output, axes=(0,))  # type:ignore
 
     # Transfer to input compute tiles, to be copied in `iobuffer`.
-    input = tile_put_sharded(input.array, compute_tiles)
+    input_sharded = tile_put_sharded(input_sharded.array, compute_tiles)
     # Blocking to make sure compute is finished.
-    input, output = tile_data_barrier(input, output)
-    return output, input.array
+    input_sharded, output = tile_data_barrier(input_sharded, output)
+
+    iobuffer = input_sharded.array
+    return output, iobuffer
 
 
 # Initialize IO buffer with zeros => first call output == 0
 iobuffer_init = np.zeros((Ntiles, N), dtype=np.float32)
-iobuffer_init = jax.device_put(iobuffer_init, jax.devices("ipu")[0])
+iobuffer_init = np.random.rand(Ntiles, N).astype(np.float32)
 
 data0 = np.random.rand(Ntiles, N).astype(np.float32)
 data1 = np.random.rand(Ntiles, N).astype(np.float32)
 
 # First run, using "zeros"
+iobuffer_init = jax.device_put(iobuffer_init, jax.devices("ipu")[0])  # [Note 1]
 output0, iobuffer0 = compute_fn(data0, iobuffer_init)
 # Second run, using `data0` previously transfered.
 output1, iobuffer1 = compute_fn(data1, iobuffer0)
 
 print("Output 0:", np.asarray(output0))
 print("Output 1:", np.asarray(output1))
+
+print("Expected output 0:", np.sum(np.sin(iobuffer_init[:, :64]), axis=-1))
 print("Expected output 1:", np.sum(np.sin(data0[:, :64]), axis=-1))
 
 print("NOTE: is iobuffer0 deleted (i.e. overwritten)?", iobuffer0.is_deleted())
+
+# Note 1: JAX bug .... Error msg: "blablah"
